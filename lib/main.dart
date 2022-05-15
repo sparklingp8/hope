e8fb595ed1f6a9d4ae3ff5a94a19eea7cff568ea
sparklingp8 <mynewnokiap8@gmail.com>
2022-05-15 17:54:31 +0530

    ch2

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,32 @@ class mainScreen extends StatelessWidget {
         title: const Text("Hope yes"),
         elevation: 0,
       ),
-  body: Container(
-    child: Text("hi hope"),
+  body: Center(
+    child: Container(
+      padding: EdgeInsets.all(2),
+      clipBehavior:  Clip.antiAlias,
+      width: 100,
+      height: 100,
+      child: const Text("I am inside box",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.bold
+      ),),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(colors: [Colors.red,Colors.yellow]),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 10,
+            ),
+          ],
+      ),
+    ),
   ),
     );
   }
